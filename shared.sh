061a7bd0deb6ec2e9d015b9976bb7e602bc2f500
rakshitmohan <81284050+rakshitmohan@users.noreply.github.com>
2022-06-15 13:55:35 +0530

    Update shared.sh

--- shared.sh
+++ shared.sh
@@ -1,4 +1,4 @@
-JIRA_URL = "https://example.atlassian.net/"  # enter url name hehreN
+JIRA_URL = "https://example.atlassian.net/"  # enter url name hehhreN
 
 JIRA_USERNAME = "test@gmail.comm"  # enter user name here
 JIRA_PASSWORD = "02A86gNk7MBhTmglawWR1BBB"  # For Jira Cloud use a token generated here: https://id.atlassian.com/manage/api-tokens
